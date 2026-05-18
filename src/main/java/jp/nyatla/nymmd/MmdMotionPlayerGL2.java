5a57aa5c0103a70af5cd2168a4b4c238c5f12d6a
mikasa <1244456115@qq.com>
2026-05-18 10:47:42 +0800

    buffer

--- src/main/java/jp/nyatla/nymmd/MmdMotionPlayerGL2.java
+++ src/main/java/jp/nyatla/nymmd/MmdMotionPlayerGL2.java
@@ -1,9 +1,6 @@
 package jp.nyatla.nymmd;
 
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.blaze3d.vertex.*;
 import jp.nyatla.nymmd.types.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
@@ -199,7 +196,8 @@ public class MmdMotionPlayerGL2 extends MmdMotionPlayer {
             // 頂点インデックスを指定してポリゴン描画
             // GL11.glDrawElements(GL11.GL_TRIANGLES, mt_ptr.indices);
 
-            // Tesselator.getInstance().end();
+            MeshData data = wr.buildOrThrow();
+            BufferUploader.drawWithShader(data);
         }
 
         GL11.glPopClientAttrib();
